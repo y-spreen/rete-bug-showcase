133023273543c51abbbc97285ef843d5251d10c3
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-26 21:04:05 -0400

    Add router.

--- src/components/App/comp.js
+++ src/components/App/comp.js
@@ -1,10 +1,23 @@
 import UploadPage from "../UploadPage/comp.vue";
 import GraphEditor from "../GraphEditor/comp.vue";
+import VueRouter from "vue-router";
+
+const routes = [
+  { path: "/editor", component: GraphEditor },
+  { path: "/upload", component: UploadPage }
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
 
 export default {
   name: "app",
-  components: {
-    UploadPage,
-    GraphEditor
+  router,
+  mounted() {
+    router.replace(routes[0].path);
   }
 };
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import App from "./components/App/comp.vue";
 import BootstrapVue from "bootstrap-vue";
 import uploader from "vue-simple-uploader";
+import VueRouter from "vue-router";
 
 import "./custom.scss";
 
 Vue.config.productionTip = false;
 Vue.use(uploader);
 Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App)
