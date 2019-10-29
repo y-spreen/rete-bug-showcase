e6936c66c03172f60208b8a65a8219d3fbe5fb5d
Yannick Spreen <yannick.spreen@gmail.com>
2019-10-28 23:29:29 -0400

    Allow pipes and wild cards, add nodes.

--- src/components/GraphEditor/comp.js
+++ src/components/GraphEditor/comp.js
@@ -10,6 +10,38 @@ import Api from "src/services/api";
 
 const mutex = new Mutex();
 
+class CustomSocket extends Rete.Socket {
+  matchesRule(str, rule) {
+    var escapeRegex = str => str.replace(/([.*+?^=!:${}()|[\]/\\])/g, "\\$1");
+    return new RegExp(
+      "^" +
+        rule
+          .split("*")
+          .map(escapeRegex)
+          .join(".*") +
+        "$"
+    ).test(str);
+  }
+  compatibleWith(otherSocket) {
+    let str1 = this.name,
+      str2 = otherSocket.name;
+
+    try {
+      str1.split("|").forEach(s1 => {
+        str2.split("|").forEach(s2 => {
+          if (s1.includes("*") && s2.includes("*")) return; // continue, cant match
+          if (s1.includes("*") && this.matchesRule(s2, s1)) throw "break";
+          if (this.matchesRule(s1, s2)) throw "break";
+        });
+      });
+    } catch (err) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
 export default {
   name: "GraphEditor",
   props: {
@@ -19,7 +51,10 @@ export default {
     return {
       loading: true,
       saveName: "My Workflow",
-      editor: null
+      editor: null,
+      classes: Array(),
+      sockets: {},
+      engine: null
     };
   },
   methods: {
@@ -35,15 +70,110 @@ export default {
       }).then(r => {
         this.editor.fromJSON(r.data);
       });
+    },
+    fetchTypes() {
+      let sockets = this.sockets;
+      Api.get("file_types").then(r => {
+        r.data
+          .map(v => v.name)
+          .forEach(v => {
+            this.sockets[v] = new CustomSocket(v);
+            this.classes.push(
+              class extends Rete.Component {
+                constructor() {
+                  super("from_data/" + v);
+                }
+
+                builder(node) {
+                  let out = new Rete.Output(1, v, sockets[v]);
+
+                  node.addOutput(out);
+                  node.data.id = "from_data/" + v;
+                  node.data.displayName = "From Data Storage";
+                }
+
+                worker(node, inputs, outputs) {
+                  // TODO
+                  outputs[1] = node.data.num;
+                }
+              }
+            );
+            this.classes.push(
+              class extends Rete.Component {
+                constructor() {
+                  super("to_data/" + v);
+                }
+
+                builder(node) {
+                  let inp = new Rete.Input(1, v, sockets[v]);
+
+                  node.addInput(inp);
+                  node.data.id = "to_data/" + v;
+                  node.data.displayName = "To Data Storage";
+                }
+
+                worker(node, inputs) {
+                  // TODO
+                  node.data.num = inputs[1];
+                }
+              }
+            );
+          });
+        this.fetchImages();
+      });
+    },
+    fetchImages() {
+      let sockets = this.sockets;
+      Api.get("list").then(r => {
+        r.data.forEach(image => {
+          this.classes.push(
+            class extends Rete.Component {
+              constructor() {
+                super("node/" + image.name);
+              }
+
+              builder(node) {
+                node.data.image = image;
+
+                let j = 1;
+                image.inputs.forEach(v => {
+                  sockets[v] = sockets[v] || new CustomSocket(v);
+                  node.addInput(new Rete.Input(j++, v, sockets[v]));
+                });
+                j = 0;
+                image.outputs.forEach(v => {
+                  sockets[v] = sockets[v] || new CustomSocket(v);
+                  node.addOutput(new Rete.Output(j++, v, sockets[v]));
+                });
+
+                node.data.id = "node/" + image.name;
+                node.data.displayName =
+                  image.name.split("/").slice(-1)[0] + " Node";
+              }
+
+              worker(node, inputs, outputs) {
+                // TODO
+                outputs[1] = node.data.num;
+              }
+            }
+          );
+        });
+        this.classes.forEach(SomeClass => {
+          const numComponent = new SomeClass();
+          this.engine.register(numComponent);
+          this.editor.register(numComponent);
+        });
+        this.loading = false;
+      });
     }
   },
   mounted() {
-    const container = document.querySelector(".rete-editor .node-editor");
-    this.editor = new Rete.NodeEditor("demo@0.1.0", container);
-    const engine = new Rete.Engine("demo@0.1.0");
+    this.editor = new Rete.NodeEditor(
+      "demo@0.1.0",
+      document.querySelector(".rete-editor .node-editor")
+    );
+    this.engine = new Rete.Engine("demo@0.1.0");
 
-    let classes = Array();
-    let sockets = {};
     const renderOptions = {
       component: CustomNodeComponent
     };
@@ -54,64 +184,14 @@ export default {
       plugins: [[VueRenderPlugin, renderOptions]]
     });
 
-    Api.get("file_types").then(r => {
-      r.data
-        .map(v => v.name)
-        .forEach(v => {
-          sockets[v] = new Rete.Socket(v);
-          classes.push(
-            class extends Rete.Component {
-              constructor() {
-                super("from_data/" + v);
-              }
-
-              builder(node) {
-                let out = new Rete.Output(1, v, sockets[v]);
-
-                node.addOutput(out);
-                node.data.id = "from_data/" + v;
-                node.data.displayName = "From Data Storage";
-              }
-
-              worker(node, inputs, outputs) {
-                outputs[1] = node.data.num;
-              }
-            }
-          );
-          classes.push(
-            class extends Rete.Component {
-              constructor() {
-                super("to_data/" + v);
-              }
-
-              builder(node) {
-                let inp = new Rete.Input(1, v, sockets[v]);
-
-                node.addInput(inp);
-                node.data.id = "to_data/" + v;
-                node.data.displayName = "To Data Storage";
-              }
-
-              worker(node, inputs) {
-                node.data.num = inputs[1];
-              }
-            }
-          );
-        });
-      classes.forEach(SomeClass => {
-        const numComponent = new SomeClass();
-        engine.register(numComponent);
-        this.editor.register(numComponent);
-      });
-      this.loading = false;
-    });
+    this.fetchTypes();
 
     this.editor.on(
       "process nodecreated noderemoved connectioncreated connectionremoved",
       async () => {
         mutex.runExclusive(async () => {
-          await engine.abort();
-          await engine.process(this.editor.toJSON());
+          await this.engine.abort();
+          await this.engine.process(this.editor.toJSON());
         });
       }
     );
